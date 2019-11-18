a080df3d94e92b7acafdd4c286d143b908833be1
razz7 <rashem2@hotmail.com>
2019-11-18 09:18:56 +0100

    a

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -1,5 +1,6 @@
 package rest;
 
+import entities.Role;
 import entities.User;
 import facades.FetchData;
 import java.io.IOException;
@@ -143,6 +144,48 @@ public class DemoResource {
             scan.close();
             return jsonStr;
         }
+    }
+    
+        @Path("makeUsers")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String makeUsers() {
+       EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.CREATE);
+    EntityManager em = emf.createEntityManager();
+    
+    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
+    // CHANGE the three passwords below, before you uncomment and execute the code below
+    // Also, either delete this file, when users are created or rename and add to .gitignore
+    // Whatever you do DO NOT COMMIT and PUSH with the real passwords
+
+    User user = new User("user1", "password1");
+    User admin = new User("admin2", "password2");
+    User both = new User("user_admin3", "password3");
+
+    if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
+      throw new UnsupportedOperationException("You have not changed the passwords");
+
+    em.getTransaction().begin();
+    Role userRole = new Role("user");
+    Role adminRole = new Role("admin");
+    user.addRole(userRole);
+    admin.addRole(adminRole);
+    both.addRole(userRole);
+    both.addRole(adminRole);
+    em.persist(userRole);
+    em.persist(adminRole);
+    em.persist(user);
+    em.persist(admin);
+    em.persist(both);
+    em.getTransaction().commit();
+    System.out.println("PW: " + user.getUserPass());
+    System.out.println("Testing user with OK password: " + user.verifyPassword("test", user.getUserPass()));
+    System.out.println("Testing user with wrong password: " + user.verifyPassword("qeasdqwe", user.getUserPass()));
+    System.out.println("Created TEST Users");
+   
+  
+        return "{\"Done\"}";  //Done manually so no need for a DTO
     }
     }
     
